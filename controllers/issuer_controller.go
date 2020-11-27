0e2b486cf490e881efb2dcdb2e09f8cad6208373
Richard Wall <richard.wall@jetstack.io>
2020-11-27 17:51:11 +0000

    Update the Issuer Ready Condition
    Signed-off-by: Richard Wall <richard.wall@jetstack.io>

--- controllers/issuer_controller.go
+++ controllers/issuer_controller.go
@@ -18,13 +18,20 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	sampleissuerv1alpha1 "github.com/cert-manager/sample-external-issuer/api/v1alpha1"
+	sampleissuerapi "github.com/cert-manager/sample-external-issuer/api/v1alpha1"
+	issuerutil "github.com/cert-manager/sample-external-issuer/internal/issuer/util"
+)
+
+const (
+	issuerReadyConditionReason = "sample-issuer.IssuerController.Reconcile"
 )
 
 // IssuerReconciler reconciles a Issuer object
@@ -37,17 +44,42 @@ type IssuerReconciler struct {
 // +kubebuilder:rbac:groups=sample-issuer.example.com,resources=issuers,verbs=get;list;watch
 // +kubebuilder:rbac:groups=sample-issuer.example.com,resources=issuers/status,verbs=get;update;patch
 
-func (r *IssuerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("issuer", req.NamespacedName)
+func (r *IssuerReconciler) Reconcile(req ctrl.Request) (result ctrl.Result, err error) {
+	ctx := context.Background()
+	log := r.Log.WithValues("issuer", req.NamespacedName)
 
-	// your logic here
+	// Get the Issuer
+	var issuer sampleissuerapi.Issuer
+	if err := r.Get(ctx, req.NamespacedName, &issuer); err != nil {
+		if err := client.IgnoreNotFound(err); err != nil {
+			return ctrl.Result{}, fmt.Errorf("unexpected get error: %v", err)
+		}
+		log.Info("Not found. Ignoring.")
+		return ctrl.Result{}, nil
+	}
 
+	// Always attempt to update the Ready condition
+	defer func() {
+		if err != nil {
+			issuerutil.SetReadyCondition(&issuer.Status, sampleissuerapi.ConditionFalse, issuerReadyConditionReason, err.Error())
+		}
+		if updateErr := r.Status().Update(ctx, &issuer); updateErr != nil {
+			err = utilerrors.NewAggregate([]error{err, updateErr})
+			result = ctrl.Result{}
+		}
+	}()
+
+	if ready := issuerutil.GetReadyCondition(&issuer.Status); ready == nil {
+		issuerutil.SetReadyCondition(&issuer.Status, sampleissuerapi.ConditionUnknown, issuerReadyConditionReason, "First seen")
+		return ctrl.Result{}, nil
+	}
+
+	issuerutil.SetReadyCondition(&issuer.Status, sampleissuerapi.ConditionTrue, issuerReadyConditionReason, "Success")
 	return ctrl.Result{}, nil
 }
 
 func (r *IssuerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&sampleissuerv1alpha1.Issuer{}).
+		For(&sampleissuerapi.Issuer{}).
 		Complete(r)
 }
---
+++ controllers/issuer_controller_test.go
@@ -0,0 +1,103 @@
+package controllers
+
+import (
+	"context"
+	"testing"
+
+	logrtesting "github.com/go-logr/logr/testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	sampleissuerapi "github.com/cert-manager/sample-external-issuer/api/v1alpha1"
+	issuerutil "github.com/cert-manager/sample-external-issuer/internal/issuer/util"
+)
+
+func TestIssuerReconcile(t *testing.T) {
+	type testCase struct {
+		name                         types.NamespacedName
+		objects                      []runtime.Object
+		expectedResult               ctrl.Result
+		expectedError                error
+		expectedReadyConditionStatus sampleissuerapi.ConditionStatus
+	}
+	tests := map[string]testCase{
+		"success": {
+			name: types.NamespacedName{Namespace: "ns1", Name: "issuer1"},
+			objects: []runtime.Object{
+				&sampleissuerapi.Issuer{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "issuer1",
+						Namespace: "ns1",
+					},
+					Status: sampleissuerapi.IssuerStatus{
+						Conditions: []sampleissuerapi.IssuerCondition{
+							{
+								Type:   sampleissuerapi.IssuerConditionReady,
+								Status: sampleissuerapi.ConditionUnknown,
+							},
+						},
+					},
+				},
+			},
+			expectedReadyConditionStatus: sampleissuerapi.ConditionTrue,
+		},
+		"issuer-not-found": {
+			name: types.NamespacedName{Namespace: "ns1", Name: "issuer1"},
+		},
+		"issuer-missing-ready-condition": {
+			name: types.NamespacedName{Namespace: "ns1", Name: "issuer1"},
+			objects: []runtime.Object{
+				&sampleissuerapi.Issuer{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "issuer1",
+						Namespace: "ns1",
+					},
+				},
+			},
+			expectedReadyConditionStatus: sampleissuerapi.ConditionUnknown,
+		},
+	}
+
+	scheme := runtime.NewScheme()
+	require.NoError(t, sampleissuerapi.AddToScheme(scheme))
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			fakeClient := fake.NewFakeClientWithScheme(scheme, tc.objects...)
+			controller := IssuerReconciler{
+				Client: fakeClient,
+				Log:    logrtesting.TestLogger{T: t},
+				Scheme: scheme,
+			}
+			result, err := controller.Reconcile(reconcile.Request{NamespacedName: tc.name})
+			if tc.expectedError != nil {
+				assertErrorIs(t, tc.expectedError, err)
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.Equal(t, tc.expectedResult, result, "Unexpected result")
+
+			if tc.expectedReadyConditionStatus != "" {
+				var issuer sampleissuerapi.Issuer
+				require.NoError(t, fakeClient.Get(context.TODO(), tc.name, &issuer))
+				assertIssuerHasReadyCondition(t, tc.expectedReadyConditionStatus, &issuer)
+			}
+		})
+	}
+}
+
+func assertIssuerHasReadyCondition(t *testing.T, status sampleissuerapi.ConditionStatus, issuer *sampleissuerapi.Issuer) {
+	condition := issuerutil.GetReadyCondition(&issuer.Status)
+	if !assert.NotNil(t, condition, "Ready condition not found") {
+		return
+	}
+	assert.Equal(t, issuerReadyConditionReason, condition.Reason, "unexpected condition reason")
+	assert.Equal(t, status, condition.Status, "unexpected condition status")
+}
